33e416f44c09a50d123516ed47de4dcd7f74ce8f
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-12 00:36:47 +0600

    Style Header And Create Simple Banner

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Banner from '../Banner/Banner'
 
 const Home = () => {
   return (
     <div className='text-black'>
-      this is homee
+      <Banner></Banner>
     </div>
   )
 }
