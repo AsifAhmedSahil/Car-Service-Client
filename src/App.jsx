e9bd54806870a8feece7a3cfca373066e33862fa
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-12 14:26:45 +0600

    Style Banner Component With Gradient Overlay

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <>
-      
-      <h1 className='bg-red-700'>sahil</h1>
-    </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
