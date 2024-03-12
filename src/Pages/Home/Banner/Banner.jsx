4c872f4af9de3cdbe060fc40e3a806522c65ee87
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-12 16:24:36 +0600

    Create A Simple Login Page With Firebase Integration

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -6,7 +6,7 @@ import img4 from "../../../assets/images/banner/4.jpg";
 
 const Banner = () => {
   return (
-    <div className="carousel w-full h-[700px]">
+    <div className="carousel w-full h-[700px] mb-20">
       <div id="slide1" className="carousel-item relative w-full">
         <img src={img1} className="w-full rounded-lg" />
         <div className="absolute rounded-lg h-full flex items-center text-start left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
