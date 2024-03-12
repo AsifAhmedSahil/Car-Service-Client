a8fed0afa6475b1cf57a7ea52aa70e2bd0d993fc
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-12 15:10:27 +0600

    Create About Section With Image Relative Position

---
+++ src/Pages/Home/About/About.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import person from "../../../assets/images/about_us/person.jpg";
+import parts from "../../../assets/images/about_us/parts.jpg";
+
+const About = () => {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row ">
+        <div className="lg:w-1/2 relative">
+          <img src={person} className="w-3/4 rounded-lg shadow-2xl" />
+          <img
+            src={parts}
+            className="w-1/2 absolute right-5 top-1/2 rounded-lg shadow-2xl border-white border-8"
+          />
+        </div>
+        <div className="lg:w-1/2 space-y-5 ">
+          <h3 className="text-orange-600 font-bold text-3xl">About Us</h3>
+          <h1 className="text-5xl font-bold">
+            We Are Experienced & Qualified In This Field
+          </h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-warning rounded-xl">Get More Info</button>
+        </div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
     <div className="carousel w-full h-[700px]">
       <div id="slide1" className="carousel-item relative w-full">
         <img src={img1} className="w-full rounded-lg" />
-        <div className="absolute h-full flex items-center left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+        <div className="absolute rounded-lg h-full flex items-center left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
           <div className="text-white space-y-7 w-1/2 pl-12">
             <h2 className="text-6xl font-bold">Affordable Price For Car Servicing</h2>
             <p>
@@ -35,6 +35,21 @@ const Banner = () => {
       </div>
       <div id="slide2" className="carousel-item relative w-full">
         <img src={img2} className="w-full" />
+        <div className="absolute rounded-lg h-full flex items-center left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+          <div className="text-white space-y-7 w-1/2 pl-12">
+            <h2 className="text-6xl font-bold">Affordable Price For Car Servicing</h2>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id
+              cumque odit nobis velit provident nulla doloribus 
+            </p>
+            <div>
+              <button className="btn btn-active btn-primary rounded-lg mr-4">Discover More</button>
+              <button className="btn btn-outline btn-secondary rounded-lg">
+                Latest Project
+              </button>
+            </div>
+          </div>
+        </div>
         <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide1" className="btn btn-circle mr-4">
             ❮
@@ -46,6 +61,21 @@ const Banner = () => {
       </div>
       <div id="slide3" className="carousel-item relative w-full">
         <img src={img3} className="w-full" />
+        <div className="absolute rounded-lg h-full flex items-center left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+          <div className="text-white space-y-7 w-1/2 pl-12">
+            <h2 className="text-6xl font-bold">Affordable Price For Car Servicing</h2>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id
+              cumque odit nobis velit provident nulla doloribus 
+            </p>
+            <div>
+              <button className="btn btn-active btn-primary rounded-lg mr-4">Discover More</button>
+              <button className="btn btn-outline btn-secondary rounded-lg">
+                Latest Project
+              </button>
+            </div>
+          </div>
+        </div>
         <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide2" className="btn btn-circle mr-4">
             ❮
@@ -57,6 +87,21 @@ const Banner = () => {
       </div>
       <div id="slide4" className="carousel-item relative w-full">
         <img src={img4} className="w-full" />
+        <div className="absolute rounded-lg h-full flex items-center left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+          <div className="text-white space-y-7 w-1/2 pl-12">
+            <h2 className="text-6xl font-bold">Affordable Price For Car Servicing</h2>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id
+              cumque odit nobis velit provident nulla doloribus 
+            </p>
+            <div>
+              <button className="btn btn-active btn-primary rounded-lg mr-4">Discover More</button>
+              <button className="btn btn-outline btn-secondary rounded-lg">
+                Latest Project
+              </button>
+            </div>
+          </div>
+        </div>
         <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide3" className="btn btn-circle mr-4">
             ❮
